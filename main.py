79006c8ecd0885bd70e17533f1b18a1648dc6fb2
bipinshambharkar <bipin0104@gmail.com>
2025-09-01 13:58:12 +0000

    python arrays and for loops learning

---
+++ main.py
@@ -0,0 +1,21 @@
+print("\n== Arrays, Matrices ==")
+A = [[1,2],[3,4]]
+print(A)
+B = [1 ,2 ,3 ,4]
+print(B)
+
+print("\n== For loop with numbers==")
+for a in [7, 6, 31, 45]:
+    print(f"Value of a: {a}")
+    print(a*2)
+    b = a+4
+    print(b)
+    
+    
+
+print("\n== For loop with strings==")
+
+for glass in ["water", "juice", "buttermilk"]:
+    print (glass)
+
+
